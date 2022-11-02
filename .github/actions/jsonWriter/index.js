c33d4855c9e388c1a2a6091865cca3263098afdc
AbdullahiAdenHassan <abdullahi.aden.hassan@gmail.com>
2022-11-02 20:24:01 +0100

    Implement json writer and clean up on json reader

---
+++ .github/actions/jsonWriter/index.js
@@ -0,0 +1,29 @@
+// Add dependencies.
+// npm install @actions/core
+// npm install @actions/github
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+const fs = require("fs");
+
+// To build.
+// npm i -g @vercel/ncc
+// ncc build .github/actions/jsonWriter/index.js -o .github/actions/jsonWriter/build
+
+//---- Methods.
+
+try {
+  const jsonFile = core.getInput("json-file");
+  const key = core.getInput("key");
+  const value = core.getInput("value");
+
+  const jsonString = fs.readFileSync(jsonFile);
+  const versions = new Map(Object.entries(JSON.parse(jsonString)));
+
+  versions.set(key, value);
+  fs.writeFileSync(jsonFile, JSON.stringify(Object.fromEntries(map)), "utf-8");
+
+  console.log(JSON.stringify(github, null, "\t"));
+} catch (error) {
+  core.setFailed(error.message);
+}
